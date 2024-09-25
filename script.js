f45eacdb4ac5c5e179e105e36ac50e13df831a03
Dheeraj <143113264+Dheeraj-2@users.noreply.github.com>
2024-09-25 13:59:14 +0530

    clone

---
+++ script.js
@@ -0,0 +1,7 @@
+//  let toggle=document.querySelector('.menu-icon')
+//  let menu=document.querySelector("#menuList")
+//  function toggleMenu()
+//  {
+//     toggle.classList.toggle()
+//     menu.classList.toggle('active')
+//  }
